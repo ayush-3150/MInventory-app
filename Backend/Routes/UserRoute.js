4bb712b71a2979dbc96c00463d85bfc73af8d9e8
Ayush <ayushgandhi3105@gmail.com>
2022-12-25 22:54:58 +0530

    Added Routes for Update usr profile and change password

--- Backend/Routes/UserRoute.js
+++ Backend/Routes/UserRoute.js
@@ -4,13 +4,19 @@ const {
   registerUser,
   loginUser,
   logoutUser,
-  getUserProfile
+  getUserProfile,
+  getLoginStatus,
+  updateProfile,
+  changePassword
 } = require('../Controllers/UserController')
 const protectMiddleware = require('../Middlewares/AuthMiddlleware')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/logout', logoutUser)
+router.get('/loginStatus', getLoginStatus)
 router.get('/profile', protectMiddleware, getUserProfile)
+router.patch('/updateProfile', protectMiddleware, updateProfile)
+router.patch('/changePassword', protectMiddleware, changePassword)
 
 module.exports = router
